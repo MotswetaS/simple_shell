e8510a3d2da9c272f93c9997c3df6a11771c8619
Rebonye <motsweta04@gmail.com>
2023-11-15 12:45:19 -0800

    new file

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+int _putchar(char c);
+void rev_string(char *s);
+char *_strcpy(char *dest, char *src);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+
+#endif
---
+++ my_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - entry point of my code
+ *
+ * print_name - Prints my name "Rebonye_Motsweta"
+ *
+ * This function will be printing my name "Rebonye_Motsweta"
+ * to the standard output
+ *
+ * Return: void
+ */
+void print_name(void)
+{
+	printf("Rebonye_Motsweta\n");
+}
